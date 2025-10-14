8d47ed63262a4b4753c45e405add64b69d3c18a2
Radu Mojic <radu.mojic@multiversx.com>
2025-10-14 17:54:15 +0300

    updated stats card config

--- src/widgets/StatsCard/StatsCard.tsx
+++ src/widgets/StatsCard/StatsCard.tsx
@@ -9,6 +9,7 @@ export interface StatsCardUIType extends WithClassnameType {
   icon?: ReactNode;
   value?: ReactNode;
   children?: ReactNode;
+  isAnimated?: boolean;
 }
 
 export const StatsCard = ({
@@ -17,6 +18,7 @@ export const StatsCard = ({
   value,
   icon,
   className,
+  isAnimated,
   children
 }: StatsCardUIType) => {
   if (!(title || value || subTitle || children)) {
@@ -30,7 +32,13 @@ export const StatsCard = ({
           <p className='text-neutral-400 mb-0 stats-card-title'>{title}</p>
         )}
         {value && (
-          <h2 className='stats-card-value mb-0 text-primary'>{value}</h2>
+          <h2
+            className={classNames('stats-card-value mb-0 text-primary', {
+              'has-animation': isAnimated
+            })}
+          >
+            {value}
+          </h2>
         )}
         {subTitle && (
           <p className='text-primary-200 mb-0 stats-card-title mt-2'>
@@ -39,7 +47,14 @@ export const StatsCard = ({
           </p>
         )}
         {children && (
-          <p className='mb-0 mt-2 text-primary-200 d-flex align-items-center'>
+          <p
+            className={classNames(
+              'text-primary-200 d-flex align-items-center stats-card-content',
+              {
+                'has-animation': isAnimated
+              }
+            )}
+          >
             {children}
           </p>
         )}
